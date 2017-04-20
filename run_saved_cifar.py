2bbc932731dc13066a94b01d24276e33b4bd11d3
Sumanth Srinivasan <sumanth.srinivasan@gmail.com>
2017-04-20 16:05:45 -0400

    code to run using saved model

---
+++ run_saved_cifar.py
@@ -0,0 +1,39 @@
+# __author__ = 'Srinivasan'
+
+from __future__ import print_function
+import keras
+from keras.datasets import cifar10
+# from keras.preprocessing.image import ImageDataGenerator
+# from keras.models import Sequential
+# from keras.layers import Dense, Dropout, Activation, Flatten
+# from keras.layers import Conv2D, MaxPooling2D
+from keras.models import model_from_json
+# import numpy
+# import scipy
+
+# The data, shuffled and split between train and test sets:
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# Convert class vectors to binary class matrices.
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
+# Load json and create model
+json_file = open('cifar_model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+
+
+# Load weights into new model
+loaded_model.load_weights("cifar_model.h5")
+print("Loaded model from disk")
+
+# # evaluate loaded model on test data
+loaded_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+score = loaded_model.evaluate(x_test, y_test, verbose=0)
+print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
